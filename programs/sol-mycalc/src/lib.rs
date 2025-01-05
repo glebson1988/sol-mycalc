f18f51f2553ed8c61bec07a748a74817c2d84905
glebson1988 <glebson1988@yandex.ru>
2025-01-05 13:40:13 +0100

    Create and Calculator structs

--- programs/sol-mycalc/src/lib.rs
+++ programs/sol-mycalc/src/lib.rs
@@ -1,16 +1,30 @@
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("DLooCc9TPNAB4EdazPb2TGzo7URacxiJ9XwZHSYTaPP7");
 
 #[program]
 pub mod sol_mycalc {
     use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn add(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator: &mut Account<'_, Calculator> = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 264)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub reminder: i64,
+}
